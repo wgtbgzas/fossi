ff58001b3a34de73a8617f283ffc1467908571ab
wgtbgzas <help@bgzashtita.es>
2026-01-13 21:20:07 +0100

    Adapt device tree for FOSSiBOT F106 Pro

--- android_device_motorola_fogorow/lineage_f106pro.mk
+++ android_device_motorola_fogorow/lineage_f106pro.mk
@@ -11,18 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from fogorow device
+# Inherit from F106 Pro device
 $(call inherit-product, device/motorola/fogorow/device.mk)
 
-PRODUCT_DEVICE := fogorow
-PRODUCT_NAME := lineage_fogorow
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g24
-PRODUCT_MANUFACTURER := motorola
+PRODUCT_DEVICE := F106_Pro
+PRODUCT_NAME := lineage_f106pro
+PRODUCT_BRAND := FOSSiBOT
+PRODUCT_MODEL := F106 Pro
+PRODUCT_MANUFACTURER := FOSSiBOT
 
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_GMS_CLIENTID_BASE := android-fossibot
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="fogorow_g-user 14 UTAS34.82-97-3 82-97-3 release-keys"
+    PRIVATE_BUILD_DESC="F106_Pro-user 14 UP1A.231005.007 2024134 release-keys"
 
-BUILD_FINGERPRINT := motorola/fogorow_g/fogorow:14/UTAS34.82-97-3/82-97-3:user/release-keys
+BUILD_FINGERPRINT := FOSSiBOT/F106_Pro_ROW/F106_Pro:14/UP1A.231005.007/2024134:user/release-keys
